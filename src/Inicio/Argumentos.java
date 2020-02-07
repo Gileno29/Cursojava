a999feb186474be24a8b103227b94e950fc57790
Gileno <gileno.cordeiroc@gmail.com>
2020-02-07 10:45:27 -0300

    passando Argumentos

---
+++ src/Inicio/Argumentos.java
@@ -0,0 +1,9 @@
+package Inicio;
+
+public class Argumentos {
+	public static void main(String[] args) {
+		System.out.println("Você digito argumentos"); 
+		
+	}
+
+}
--- src/Inicio/FirstProgram.java
+++ src/Inicio/FirstProgram.java
@@ -3,7 +3,7 @@ package Inicio;
 public class FirstProgram {
 	public static void main(String[] args) {
 		
-		System.out.println("Ola Mundo!");
+		System.out.println("Hello World");
 		
 	}
 
